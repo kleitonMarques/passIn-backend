6849f37da37530945c411d2615ede6b6c178670c
Kleiton Marques <kleitonm.marques@gmail.com>
2024-04-09 21:49:20 -0300

    add two await within the same Promise all

--- src/routes/register-for-event.ts
+++ src/routes/register-for-event.ts
@@ -36,17 +36,19 @@ export async function registerForEvent(app: FastifyInstance) {
       throw new Error('This e-mail is already registered for this event.')
     }
 
-    const event = await prisma.event.findUnique({
-      where: {
-        id: eventId,
-      }
-    })
+    const [event, amountOfAttendeesForEvent] = await Promise.all([
+      prisma.event.findUnique({
+        where: {
+          id: eventId,
+        }
+      }),
 
-    const amountOfAttendeesForEvent = await prisma.attendee.count({
-      where: {
-        eventId,
-      }
-    })
+      prisma.attendee.count({
+        where: {
+          eventId,
+        }
+      })
+    ])
 
     if (event?.maximumAttendees && amountOfAttendeesForEvent >= event?.maximumAttendees) {
       throw new Error('The maximum number of attendees for this event has been reached.')
